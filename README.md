f4ddbfcb06a9081d31eb05fcd4e150508b79babb
alreylz <alreylop@gmail.com>
2024-09-13 17:19:50 +0200

    Added super simple script to auto generate root README to point to all projects found in the projects folder

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# react-playground
+# React Playground 🕹️🗒️
+
+- [00-hello-world](./projects/00-hello-world/README.md)
+- [01-noughts-and-crosses](./projects/01-noughts-and-crosses/README.md)
---
+++ autoReadme.js
@@ -0,0 +1,25 @@
+import fs from 'fs/promises';
+
+const basicContent = "# React Playground 🕹️🗒️";
+
+
+
+(async () =>{
+
+    //Read number of folders in the projects directory
+
+   const projectsList = await fs.readdir('./projects');
+
+
+    let content  = `${basicContent}\n\n`;
+    projectsList.forEach( (val) => {
+        content += `- [${val}](./projects/${val}/README.md)\n`
+    })
+
+
+
+
+    // Write to the Readme
+    await fs.writeFile('./README.md', content, 'utf8');
+
+})()
